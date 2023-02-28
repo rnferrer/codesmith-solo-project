511df8e4578f3d939259e62a773f6670063c495c
rnferrer <nathanferrer29@yahoo.com>
2023-02-28 11:36:43 -0800

    created base file structure

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+//require('dotenv').config();
+
+
+console.log('on port 3000')
+app.listen(3000)
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    compress: true,
+    port: 8080,
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+      publicPath: '/build'
+    },
+    proxy: {
+      //localhost:8080/api/leaders
+      '/*': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      }
+    }
+  },
+  module:{
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {targets: "defaults"}],
+              ['@babel/preset-react', {targets: "defaults"}]
+            ]
+          }
+        }
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'index.html'
+  })]
+}
